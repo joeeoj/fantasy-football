f19e2937d5f7a51981d66c7f6c571606db7aaf3d
joeeoj <joeeoj@blueskyemail.net>
2021-09-07 09:29:36 -0700

    lineup parser and current week1 rosters

---
+++ code/get_lineups.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+import csv
+from datetime import date
+from itertools import chain
+import os
+from pathlib import Path
+
+from espn_api.football import League
+from espn_api.football.box_player import BoxPlayer
+from espn_api.football.box_score import BoxScore
+
+
+LEAGUE_ID = 17588244
+DATA_DIR = Path(__file__).parent.parent / 'data'
+YEAR = date.today().year if date.today().month in [9, 10, 11, 12] else (date.today().year - 1)
+NOT_PLAYING = set(['BE', 'IR'])  # non-playing slots
+# RB/WR/TE = FLEX
+# we do not have OP, RB/WR, WR/TE, or Rookie
+LEAGUE_SLOTS = {
+    'QB': 1,
+    'RB': 2,
+    'WR': 3,
+    'TE': 1,
+    'RB/WR/TE': 1,
+    'K': 1,
+    'DST': 1,
+    'BE': 5,
+    'IR': 1,
+}
+
+
+def parse_player_data(p: BoxPlayer) -> dict:
+    slots = set([e for e in p.eligibleSlots if (e in LEAGUE_SLOTS.keys() and e not in NOT_PLAYING)])
+
+    return {
+        'player_id': p.playerId,
+        'player_name': p.name,
+        'pos': p.position,
+        'team': p.proTeam,
+        'projected_points': p.projected_points,
+        'current_position': p.slot_position,
+        'QB': 'QB' in slots, 
+        'RB': 'RB' in slots, 
+        'WR': 'WR' in slots, 
+        'TE': 'TE' in slots, 
+        'RB/WR/TE': 'RB/WR/TE' in slots, 
+        'K': 'K' in slots, 
+        'DST': 'DST' in slots,
+    }
+
+
+def parse_lineups(box_score: BoxScore, week: int) -> list[BoxPlayer]:
+    home_team = box_score.home_team
+    home_lineup = box_score.home_lineup
+
+    away_team = box_score.away_team
+    away_lineup = box_score.away_lineup
+
+    players = []
+    for player in home_lineup:
+        p = parse_player_data(player)
+        p['week'] = week
+        p['fantasy_team_id'] = home_team.team_id
+        p['fantasy_team_name'] = home_team.team_name
+        p['opponent_team_id'] = away_team.team_id
+        p['opponent_team_name'] = away_team.team_name
+        p['home_away'] = 'home'
+        players.append(p)
+
+    for player in away_lineup:
+        p = parse_player_data(player)
+        p['week'] = week
+        p['fantasy_team_id'] = away_team.team_id
+        p['fantasy_team_name'] = away_team.team_name
+        p['opponent_team_id'] = home_team.team_id
+        p['opponent_team_name'] = home_team.team_name
+        p['home_away'] = 'away'
+        players.append(p)
+
+    return players
+
+
+if __name__ == '__main__':
+    league = League(league_id=LEAGUE_ID, year=YEAR)
+    week = league.current_week
+    print(f'Current week: {week}')
+
+    box_scores = league.box_scores()
+    lineups = list(chain.from_iterable([parse_lineups(bs, week) for bs in box_scores]))
+    print(f'Players parsed: {len(lineups):,}')
+
+    fout = DATA_DIR / f'lineups_week_{week:02}.csv'
+    print(f'Saving to {fout.name}')
+    with open(fout, 'wt') as f:
+        csvwriter = csv.DictWriter(f, fieldnames=lineups[0].keys())
+        csvwriter.writeheader()
+        csvwriter.writerows(lineups)
